80b652d357a65d3b25a10ecafd31040c9d58a81b
Prathit Visen <prathitvisenthemaker@gmail.com>
2024-04-21 16:31:53 +0530

    commit

--- fin_web/routes.py
+++ fin_web/routes.py
@@ -64,13 +64,15 @@ def logout():
     return redirect(url_for("home_page"))
 
 
+#ADDING----DATA
+
 
 @app.route("/add")
 def adding():
     return render_template('add.html')
 
-"""
-@app.route('/addexpense',methods=['GET', 'POST'])
+
+"""@app.route('/addexpense',methods=['GET', 'POST'])
 def addexpense():
     
     date = request.form['date']
@@ -85,4 +87,263 @@ def addexpense():
     print(date + " " + expensename + " " + amount + " " + paymode + " " + category)
     
     return redirect("/display")
-"""
+
+
+
+#DISPLAY---graph 
+
+@app.route("/display")
+def display():
+    print(session["username"],session['id'])
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM expenses WHERE userid = % s AND date ORDER BY `expenses`.`date` DESC',(str(session['id'])))
+    expense = cursor.fetchall()
+  
+       
+    return render_template('display.html' ,expense = expense)
+                          
+
+
+
+#delete---the--data
+
+@app.route('/delete/<string:id>', methods = ['POST', 'GET' ])
+def delete(id):
+     cursor = mysql.connection.cursor()
+     cursor.execute('DELETE FROM expenses WHERE  id = {0}'.format(id))
+     mysql.connection.commit()
+     print('deleted successfully')    
+     return redirect("/display")
+ 
+    
+#UPDATE---DATA
+
+@app.route('/edit/<id>', methods = ['POST', 'GET' ])
+def edit(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM expenses WHERE  id = %s', (id,))
+    row = cursor.fetchall()
+   
+    print(row[0])
+    return render_template('edit.html', expenses = row[0])
+
+
+
+
+@app.route('/update/<id>', methods = ['POST'])
+def update(id):
+  if request.method == 'POST' :
+   
+      date = request.form['date']
+      expensename = request.form['expensename']
+      amount = request.form['amount']
+      paymode = request.form['paymode']
+      category = request.form['category']
+    
+      cursor = mysql.connection.cursor()
+       
+      cursor.execute("UPDATE `expenses` SET `date` = % s , `expensename` = % s , `amount` = % s, `paymode` = % s, `category` = % s WHERE `expenses`.`id` = % s ",(date, expensename, amount, str(paymode), str(category),id))
+      mysql.connection.commit()
+      print('successfully updated')
+      return redirect("/display")
+     
+      
+
+            
+ 
+         
+    
+            
+ #limit
+@app.route("/limit" )
+def limit():
+       return redirect('/limitn')
+
+@app.route("/limitnum" , methods = ['POST' ])
+def limitnum():
+     if request.method == "POST":
+         number= request.form['number']
+         cursor = mysql.connection.cursor()
+         cursor.execute('INSERT INTO limits VALUES (NULL, % s, % s) ',(session['id'], number))
+         mysql.connection.commit()
+         return redirect('/limitn')
+     
+         
+@app.route("/limitn") 
+def limitn():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT limitss FROM `limits` ORDER BY `limits`.`id` DESC LIMIT 1')
+    x= cursor.fetchone()
+    s = x[0]
+    
+    
+    return render_template("limit.html" , y= s)
+
+#REPORT
+
+@app.route("/today")
+def today():
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT TIME(date)   , amount FROM expenses  WHERE userid = %s AND DATE(date) = DATE(NOW()) ',(str(session['id'])))
+      texpense = cursor.fetchall()
+      print(texpense)
+      
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT * FROM expenses WHERE userid = % s AND DATE(date) = DATE(NOW()) AND date ORDER BY `expenses`.`date` DESC',(str(session['id'])))
+      expense = cursor.fetchall()
+  
+      total=0
+      t_food=0
+      t_entertainment=0
+      t_business=0
+      t_rent=0
+      t_EMI=0
+      t_other=0
+ 
+     
+      for x in expense:
+          total += x[4]
+          if x[6] == "food":
+              t_food += x[4]
+            
+          elif x[6] == "entertainment":
+              t_entertainment  += x[4]
+        
+          elif x[6] == "business":
+              t_business  += x[4]
+          elif x[6] == "rent":
+              t_rent  += x[4]
+           
+          elif x[6] == "EMI":
+              t_EMI  += x[4]
+         
+          elif x[6] == "other":
+              t_other  += x[4]
+            
+      print(total)
+        
+      print(t_food)
+      print(t_entertainment)
+      print(t_business)
+      print(t_rent)
+      print(t_EMI)
+      print(t_other)
+
+
+     
+      return render_template("today.html", texpense = texpense, expense = expense,  total = total ,
+                           t_food = t_food,t_entertainment =  t_entertainment,
+                           t_business = t_business,  t_rent =  t_rent, 
+                           t_EMI =  t_EMI,  t_other =  t_other )
+     
+
+@app.route("/month")
+def month():
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT DATE(date), SUM(amount) FROM expenses WHERE userid= %s AND MONTH(DATE(date))= MONTH(now()) GROUP BY DATE(date) ORDER BY DATE(date) ',(str(session['id'])))
+      texpense = cursor.fetchall()
+      print(texpense)
+      
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT * FROM expenses WHERE userid = % s AND MONTH(DATE(date))= MONTH(now()) AND date ORDER BY `expenses`.`date` DESC',(str(session['id'])))
+      expense = cursor.fetchall()
+  
+      total=0
+      t_food=0
+      t_entertainment=0
+      t_business=0
+      t_rent=0
+      t_EMI=0
+      t_other=0
+ 
+     
+      for x in expense:
+          total += x[4]
+          if x[6] == "food":
+              t_food += x[4]
+            
+          elif x[6] == "entertainment":
+              t_entertainment  += x[4]
+        
+          elif x[6] == "business":
+              t_business  += x[4]
+          elif x[6] == "rent":
+              t_rent  += x[4]
+           
+          elif x[6] == "EMI":
+              t_EMI  += x[4]
+         
+          elif x[6] == "other":
+              t_other  += x[4]
+            
+      print(total)
+        
+      print(t_food)
+      print(t_entertainment)
+      print(t_business)
+      print(t_rent)
+      print(t_EMI)
+      print(t_other)
+
+
+     
+      return render_template("today.html", texpense = texpense, expense = expense,  total = total ,
+                           t_food = t_food,t_entertainment =  t_entertainment,
+                           t_business = t_business,  t_rent =  t_rent, 
+                           t_EMI =  t_EMI,  t_other =  t_other )
+         
+@app.route("/year")
+def year():
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT MONTH(date), SUM(amount) FROM expenses WHERE userid= %s AND YEAR(DATE(date))= YEAR(now()) GROUP BY MONTH(date) ORDER BY MONTH(date) ',(str(session['id'])))
+      texpense = cursor.fetchall()
+      print(texpense)
+      
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT * FROM expenses WHERE userid = % s AND YEAR(DATE(date))= YEAR(now()) AND date ORDER BY `expenses`.`date` DESC',(str(session['id'])))
+      expense = cursor.fetchall()
+  
+      total=0
+      t_food=0
+      t_entertainment=0
+      t_business=0
+      t_rent=0
+      t_EMI=0
+      t_other=0
+ 
+     
+      for x in expense:
+          total += x[4]
+          if x[6] == "food":
+              t_food += x[4]
+            
+          elif x[6] == "entertainment":
+              t_entertainment  += x[4]
+        
+          elif x[6] == "business":
+              t_business  += x[4]
+          elif x[6] == "rent":
+              t_rent  += x[4]
+           
+          elif x[6] == "EMI":
+              t_EMI  += x[4]
+         
+          elif x[6] == "other":
+              t_other  += x[4]
+            
+      print(total)
+        
+      print(t_food)
+      print(t_entertainment)
+      print(t_business)
+      print(t_rent)
+      print(t_EMI)
+      print(t_other)
+
+     
+      return render_template("today.html", texpense = texpense, expense = expense,  total = total ,
+                           t_food = t_food,t_entertainment =  t_entertainment,
+                           t_business = t_business,  t_rent =  t_rent, 
+                           t_EMI =  t_EMI,  t_other =  t_other )
+      """
