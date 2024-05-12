44a4cf73eb3a8adec36ba8d4b31e0e2d13e04270
Prathit Visen <75821076+DEADPOOLpv@users.noreply.github.com>
2024-05-12 19:11:54 +0530

    Update __init__.py

--- fin_web/__init__.py
+++ fin_web/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finmag.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
